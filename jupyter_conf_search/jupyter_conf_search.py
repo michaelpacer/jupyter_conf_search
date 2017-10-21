1f789de3daa9b75565ac64ea507a755d6f738125
M Pacer <mpacer@berkeley.edu>
2017-10-20 17:37:35 -0700

    refactor code to reuse logic & not search recursively for local files

--- jupyter_conf_search/jupyter_conf_search.py
+++ jupyter_conf_search/jupyter_conf_search.py
@@ -4,6 +4,8 @@ import os
 import sys
 import re
 
+canonical_names_regex = re.compile(r"jupyter_(\w*_|)config")
+
 def valid_conf_file(file_name):
 # replace with canonical config validation checker
     if not os.path.isfile(file_name):
@@ -12,35 +14,59 @@ def valid_conf_file(file_name):
         return True
     if os.path.splitext(file_name)[1]=='.json':
         return True
-    
-
-canonical_names_regex = re.compile(r"jupyter_(\w*_|)config")
 
 def valid_local_conf_file(file_path):
     file_name = os.path.splitext(os.path.split(file_path)[1])[0]
     return valid_conf_file(file_path) and canonical_names_regex.match(file_name)
-    
+
+def directory_validator(directory, filter_func, recursive=True):
+    """
+    Creates path to consumed by glob in order to search for configuration
+    files meeting various requirements.
+
+    Arguments
+    ---------
+    directory: string or Path
+        directory from which to begin search
+    filter_func: function
+        function used to determine whether file should be included
+    recursive: bool
+        if true, applies recursive beneathe directory
+    """
+    if recursive:
+        path_to_check = os.path.join(directory, "**")
+    else:
+        path_to_check = os.path.join(directory, "*")
+    potential_files = glob.glob(path_to_check, recursive=recursive)
+    return [f for f in potential_files if filter_func(f)]
+
 def search_jupyter_paths(search_term=''):
-    
-    conf_path_list = []
-    for dir in jpaths.jupyter_config_path():
-        conf_path_list.extend(glob.glob(dir+"/**", recursive=True))
-        
-    conf_file_list = [f for f in conf_path_list if valid_conf_file(f)]
-    local_path_list = glob.glob(os.getcwd()+"/**", recursive=True)
-    
-    conf_file_list.extend([f for f in local_path_list if valid_local_conf_file(f)])
-    
-    # go through files, 
+
+    conf_file_list= []
+    for directory in jpaths.jupyter_config_path():
+        conf_file_list.extend([
+            f for f in directory_validator(directory, valid_conf_file)
+        ])
+
+    conf_file_list.extend([
+        f for f in directory_validator(
+            os.getcwd(),
+            valid_local_conf_file,
+            recursive=False
+            )
+    ])
+
+
+    # go through files,
     # if search term found in file
-    # print name, line_no, content 
+    # print name, line_no, content
     conf_file_list.reverse()
     for file_name in conf_file_list:
-        if len(search_term)>0:
+        if search_term:
             print_indexed_content(file_name=file_name, search_term=search_term)
         else:
             print(file_name)
-        
+
 def print_indexed_content(file_name='', search_term=''):
     with open(file_name,"r") as f:
         if search_term in f.read():
